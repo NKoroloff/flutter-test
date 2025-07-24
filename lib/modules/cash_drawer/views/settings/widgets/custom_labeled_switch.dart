1bc66de5f203de9c30a15ca7d67fb71d4bc211cc
serbs <1123@gmail.com>
2025-07-24 13:06:07 +0300

    added disabled to switch

--- lib/modules/cash_drawer/views/settings/widgets/custom_labeled_switch.dart
+++ lib/modules/cash_drawer/views/settings/widgets/custom_labeled_switch.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CustomLabeledSwitch extends StatelessWidget {
@@ -6,19 +7,21 @@ class CustomLabeledSwitch extends StatelessWidget {
     required this.onChanged,
     required this.label,
     required this.value,
+    this.disabled = false,
     super.key,
   });
   final void Function(bool) onChanged;
   final String label;
   final bool value;
+  final bool disabled;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Text(label.tr),
+        Text(label.tr, style: TextStyle(color: disabled ? Colors.grey : Colors.black)),
         const Spacer(),
-        CupertinoSwitch(value: value, onChanged: onChanged),
+        CupertinoSwitch(value: value, onChanged: disabled == true ? null : onChanged),
         const SizedBox(width: 20),
       ],
     );
